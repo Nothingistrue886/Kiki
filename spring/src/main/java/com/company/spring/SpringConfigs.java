b04451a91da4d249114715947a4585261bd728ea
Nothingistrue886 <18617488187@163.com>
2023-10-10 17:15:46 +0800

    update

--- spring/src/main/java/com/company/spring/SpringConfigs.java
+++
@@ -1,9 +0,0 @@
-package com.company.spring;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.company.spring.beans")
-public class SpringConfigs {
-}
