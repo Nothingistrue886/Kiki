038f5219189e75baeaaa02eb4915fc0f5f13e331
Nothingistrue886 <18617488187@163.com>
2023-11-09 21:18:11 +0800

    update

--- study-java-springboot/src/main/java/com/czff/study/SpringbootApplication.java
+++ study-java-springboot/src/main/java/com/czff/study/SpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.czff.mydemo;
+package com.czff.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- study-java-springboot/src/test/java/com/czff/study/SpringbootApplicationTests.java
+++ study-java-springboot/src/test/java/com/czff/study/SpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.czff.mydemo;
+package com.czff.study;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
